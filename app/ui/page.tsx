1528a7bd0b0db3d518b9d7de4fa2d41e2cc5a3c5
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-09-30 17:50:21 -0500

    Revert to previous stable state and clean up untracked files

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import SideNav from '@/components/Sidenav';
-import NavLink from '@/components/NavLink';
-import SignOutButton from '@/components/SignOutButton';
-
 
 export default function UIPage({ children }: { children: React.ReactNode }) {
   return (
